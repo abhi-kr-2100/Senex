9d9297c50b302e3846f4ac610ea3b8cbb3b8f30c
Abhishek Kumar <abhi.kr.2100@gmail.com>
2022-10-25 19:39:15 +0530

    Add Mockito and Jsoup as dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,11 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter:5.9.0")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.0")
+
+    testImplementation("org.mockito:mockito-inline:4.8.0")
+    testImplementation("org.mockito:mockito-junit-jupiter:4.8.0")
+
+    implementation("org.jsoup:jsoup:1.15.3")
 }
 
 tasks.getByName<Test>("test") {
