e70a28c5bc4291bc4356bf75981d0692410cf256
Abhishek Kumar <abhi.kr.2100@gmail.com>
2022-10-26 22:32:03 +0530

    Complete the implementation of URLGenerator
    Instead of generating just a single URL, URLGenerator now successfully
    generates every possible URL from the given combinations of query
    parameters.

--- src/main/java/io/abhikr2100/URLGenerator.java
+++ src/main/java/io/abhikr2100/URLGenerator.java
@@ -3,6 +3,7 @@ package io.abhikr2100;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -25,17 +26,50 @@ public class URLGenerator {
         }
     }
 
-    public static ArrayList<URL> generate(HashMap<String, ArrayList<String>> argsMap) {
-        HashMap<String, String> queryMap = new HashMap<>();
+    private static ArrayList<HashMap<String, String>> generateQueryMapsAux(
+            HashMap<String, ArrayList<String>> argsMap, HashMap<String, String> prefix
+    ) {
+        HashMap<String, String> prefixCopy = new HashMap<>(prefix);
+
         for (Map.Entry<String, ArrayList<String>> arg : argsMap.entrySet()) {
             String key = arg.getKey();
-            String val = arg.getValue().get(0);
+            ArrayList<String> values = arg.getValue();
 
-            queryMap.put(key, val);
+            if (prefixCopy.containsKey(key)) {
+                continue;
+            }
+
+            if (values.size() == 1) {
+                prefixCopy.put(key, values.get(0));
+                continue;
+            }
+
+            ArrayList<HashMap<String, String>> queryMaps = new ArrayList<>();
+            for (String value : values) {
+                HashMap<String, String> newPrefix = new HashMap<>(prefixCopy);
+                newPrefix.put(key, value);
+
+                queryMaps.addAll(generateQueryMapsAux(argsMap, newPrefix));
+            }
+
+            return queryMaps;
         }
 
-        ArrayList<URL> urls = new ArrayList<>();
-        urls.add(URLGenerator.generateURLFromQueryMap(queryMap));
+        return new ArrayList<>(Collections.singletonList(prefixCopy));
+    }
+
+    private static ArrayList<HashMap<String, String>> generateQueryMaps(HashMap<String, ArrayList<String>> argsMap) {
+        HashMap<String, String> initialPrefix = new HashMap<>();
+        return generateQueryMapsAux(argsMap, initialPrefix);
+    }
+
+    public static ArrayList<URL> generate(HashMap<String, ArrayList<String>> argsMap) {
+        ArrayList<HashMap<String, String>> queryMaps = generateQueryMaps(argsMap);
+
+        ArrayList<URL> urls = new ArrayList<>(queryMaps.size());
+        for (HashMap<String, String> queryMap : queryMaps) {
+            urls.add(generateURLFromQueryMap(queryMap));
+        }
 
         return urls;
     }
