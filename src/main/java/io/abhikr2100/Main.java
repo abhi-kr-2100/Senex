d569c245ba8d58b00bfa4eeb1bbc96461b8db8dc
Abhishek Kumar <abhi.kr.2100@gmail.com>
2022-10-27 20:03:17 +0530

    Implement Main to scrap Tatoeba

--- src/main/java/io/abhikr2100/Main.java
+++ src/main/java/io/abhikr2100/Main.java
@@ -1,7 +1,14 @@
 package io.abhikr2100;
 
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        HashMap<String, ArrayList<String>> argsMap = ArgsParser.parse(args);
+        ArrayList<URL> urls = URLGenerator.generate(argsMap);
+        ConcurrentTatoebaSentenceExtractor senex = new ConcurrentTatoebaSentenceExtractor(System.out);
+        senex.extractAll(urls);
     }
 }
